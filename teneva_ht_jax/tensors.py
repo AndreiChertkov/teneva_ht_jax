e6522f223bb8f7875a8bebd0e857d409f0b0d318
AndreiChertkov <andre.chertkov@gmail.com>
2023-04-21 13:47:11 +0300

    Update "tensors" module

--- teneva_ht_jax/tensors.py
+++ teneva_ht_jax/tensors.py
@@ -24,30 +24,33 @@ def rand(d, n, r, key, a=-1., b=1.):
         list: HT-tensor.
 
     """
-    q = d.bit_length() - 1
+    q = d.bit_length() # Full number of levels (e.g., d=8 -> q=4)
 
     if isinstance(r, int):
-        r = [r] * q
+        r = [r] * (q-1)
 
-    if len(r) != q:
+    if len(r) != (q-1):
         raise ValueError('Invalid length of ranks list')
 
     Y = []
 
-    key, key_cur = jax.random.split(key)
-    Y_cur = jax.random.uniform(key_cur, (2**q, n, r[0]), minval=a, maxval=b)
-    Y.append(Y_cur)
-
-    for k in range(1, q+1):
+    def _rand_level(key, sh):
         key, key_cur = jax.random.split(key)
+        Yl = jax.random.uniform(key_cur, sh, minval=a, maxval=b)
+        return Yl, key
 
-        if k < q:
-            Y_cur = jax.random.uniform(key_cur,
-                (2**(q-k), r[k-1], r[k], r[k-1]), minval=a, maxval=b)
-        else:
-            Y_cur = jax.random.uniform(key_cur,
-                (r[k-1], r[k-1]), minval=a, maxval=b)
+    # Build the first level (leafs):
+    Yl, key = _rand_level(key, sh=(d, n, r[0]))
+    Y.append(Yl) # 3D tensor (len, n, r_up)
 
-        Y.append(Y_cur)
+    # Build the inner levels:
+    for k in range(1, q-1):
+        dl = 2**(q-k-1) # Length of the current layer
+        Yl, key = _rand_level(key, sh=(dl, r[k-1], r[k], r[k-1]))
+        Y.append(Yl) # 4D tensor (len, r_down, r_up, r_down)
+
+    # Build the last level (root):
+    Yl, key = _rand_level(key, sh=(r[-1], r[-1]))
+    Y.append(Yl) # 2D tensor (r_down, r_down)
 
     return Y
